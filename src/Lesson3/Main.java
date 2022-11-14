f9e8bd3fdc3e86b6b03c35c4474395b22af358cd
Alex Bolodurin <shur026@yandex.ru>
2022-11-14 14:56:38 +0300

    Corrections done.

--- src/Lesson3/Main.java
+++ src/Lesson3/Main.java
@@ -6,11 +6,14 @@ import Lesson3.MyExceptions.MyArraySizeException;
 public class Main {
     public static void main(String[] args) {
         String[][] array = {{"1", "2", "3", "3"},
-                            {"4", "5", "6", "4"},
-                            {"7", "8", "9", "3"},
-                            {"7", "8", "g", "3"}};
-        System.out.println(getSumFromArray(array));
-
+                {"4", "5", "6", "7"},
+                {"7", "8", "9", "3"},
+                {"7", "8", "g", "3"}};
+        try {
+            System.out.println(getSumFromArray(array));
+        } catch (MyArraySizeException | MyArrayDataException myException) {
+            System.out.println(myException.getMessage());
+        }
     }
 
     public static int getSumFromArray(String[][] array) {
@@ -18,6 +21,9 @@ public class Main {
         int columnSize = array[rowSize - 1].length;
         if (rowSize != 4 || columnSize != 4) {
             throw new MyArraySizeException();
+        }
+        for (String[] strings : array) {
+            if (strings.length != 4) throw new MyArraySizeException();
         }
 
         int sum = 0;
