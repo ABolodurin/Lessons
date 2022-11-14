9ebc1871889bf357f10a6900f7f6163e5e5dd2ec
Alex Bolodurin <shur026@yandex.ru>
2022-11-14 22:36:39 +0300

    Lesson5 complete.

---
+++ src/Lesson5/TelephoneBook/TelephoneBookException.java
@@ -0,0 +1,7 @@
+package Lesson5.TelephoneBook;
+
+public class TelephoneBookException extends RuntimeException {
+    public TelephoneBookException(String message) {
+        super(message);
+    }
+}
